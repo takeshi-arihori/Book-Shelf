90355f97c83b9a9ab87ed6d9d924b008ad485e37
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2025-06-14 23:24:01 +0900

    fix: main.tsxから不要なReduxのProviderを削除

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import App from './App';
-import { store } from './store/store';
 import { SearchProvider } from './store/contexts/SearchContext';
 import { ToastProvider } from './store/contexts/ToastContext';
 import { AuthProvider } from './store/contexts/AuthContext';
@@ -11,7 +9,6 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
       <SearchProvider>
         <ToastProvider>
           <AuthProvider>
@@ -21,6 +18,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           </AuthProvider>
         </ToastProvider>
       </SearchProvider>
-    </Provider>
   </React.StrictMode>
 );
